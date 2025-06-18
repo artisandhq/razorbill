1f3ea364f41a8ef1bb9af3298f079c49e13ab604
richardoey <richfir@gmail.com>
2025-06-18 22:20:53 +0800

    update registerType

--- vite.config.ts
+++ vite.config.ts
@@ -7,31 +7,43 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate', // or 'prompt'
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      registerType: 'prompt',
+      includeAssets: ['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
       manifest: {
-        name: 'My React PWA',
-        short_name: 'React PWA',
-        description: 'A React PWA built with Vite',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
+        name: "React-vite-app",
+        short_name: "react-vite-app",
+        description: "I am a simple vite app",
+        icons: [{
+          src: '/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'favicon'
+        },
+        {
+          src: '/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'favicon'
+        },
+        {
+          src: '/apple-touch-icon.png',
+          sizes: '180x180',
+          type: 'image/png',
+          purpose: 'apple touch icon',
+        },
+        {
+          src: '/maskable_icon.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        }
         ],
-      },
+        theme_color: '#171717',
+        background_color: '#f0e7db',
+        display: "standalone",
+        scope: '/',
+        start_url: "/",
+        orientation: 'portrait'
+      }
     }),],
 })
