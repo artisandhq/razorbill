7fe6c4162f4b0d20e69f8366692f73467f7bb2b6
richardoey <richfir@gmail.com>
2025-06-29 10:38:17 +0800

    update page component name

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,18 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import AppLayout from "./AppLayout.tsx";
-import Todo from "./Todo.tsx";
-import Message from "./Message.tsx";
-import Me from "./Me.tsx";
+import Stats from "./Stats.tsx";
+import Accounts from "./Accounts.tsx";
+import Settings from "./Settings.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route element={<AppLayout />}>
         <Route index element={<App />} />
-        <Route path="todo" element={<Todo />} />
-        <Route path="message" element={<Message />} />
-        <Route path="me" element={<Me />} />
+        <Route path="stats" element={<Stats />} />
+        <Route path="accounts" element={<Accounts />} />
+        <Route path="settings" element={<Settings />} />
       </Route>
     </Routes>
   </BrowserRouter>
